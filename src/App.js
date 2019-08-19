168708058f296396c8fd0d922224c005cadaca27
gregatgit <gregdd@outlook.com>
2019-08-19 13:20:44 +1000

    added stylings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Header from './components/Header'
+import './app.scss'
+
 
 function App() {
   return (
     <div className="App">
-      My App
+      <Header />
     </div>
   );
 }
