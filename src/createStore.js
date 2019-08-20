fa2b598c5d496b9b785d2b0aacc7c02a355b625f
gregatgit <gregdd@outlook.com>
2019-08-20 13:30:26 +1000

    add redux devtools

--- src/createStore.js
+++ src/createStore.js
@@ -6,4 +6,5 @@ export const middlewares = [ReduxThunk]
 
 export const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
 
-export const store = createStoreWithMiddleware(RootReducer)
+export const store = createStoreWithMiddleware(RootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__())
